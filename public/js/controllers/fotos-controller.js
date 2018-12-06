0f60fd3497361a691768d6c20771efdc733070dd
Rodrigo Felix <rodrigo.felix321@gmail.com>
2018-12-06 01:17:50 +0000

    Aula 03 - Tornando nossa aplicação mais próxima de uma aplicação real

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,14 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-    $scope.foto = {
-        titulo : 'Leão',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+    $scope.foto = [];
+
+    $http.get('v1/fotos')
+    .success(function(retorno) {
+        console.log(retorno);
+        $scope.fotos = retorno;
+    })
+    .error(function(error) {
+        console.log(error);
+    })
 
 });
